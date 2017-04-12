ede031df6be938f4eac8b03236e7cfd3d9d27582
Ray <ray.li.verified@gmail.com>
2017-04-11 20:15:51 -0500

    Consolidate Adapter File
    *Move Listview Adapter to CustomAlertDialogue file so all code is in one file.

--- customalert/src/main/java/stream/customdialogue/CustomAlertDialogue.java
+++ customalert/src/main/java/stream/customdialogue/CustomAlertDialogue.java
@@ -22,11 +22,13 @@ import android.view.ViewStub;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CustomAlertDialogue extends DialogFragment {
     public static final String TAG = CustomAlertDialogue.class.getSimpleName();
@@ -594,5 +596,69 @@ public class CustomAlertDialogue extends DialogFragment {
         DIALOGUE,
         ACTIONSHEET,
         SELECTOR
+    }
+
+    public class CustomActionsheetAdapter extends BaseAdapter {
+
+        private List<String> mDatas;
+        private List<String> mDestructive;
+
+        public CustomActionsheetAdapter(List<String> datas, List<String> destructive){
+            this.mDatas =datas;
+            this.mDestructive =destructive;
+        }
+
+        @Override
+        public int getCount() {
+            return mDatas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDatas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String data= mDatas.get(position);
+            Holder holder=null;
+            View view =convertView;
+            if(view==null){
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                view=inflater.inflate(R.layout.alertbutton, null);
+                holder= createHolder(view);
+                view.setTag(holder);
+            }
+            else{
+                holder=(Holder) view.getTag();
+            }
+            holder.UpdateUI(parent.getContext(),data,position);
+            return view;
+        }
+        public Holder createHolder(View view){
+            return new Holder(view);
+        }
+
+        class Holder {
+            private TextView buttonText;
+
+            public Holder(View view){
+                buttonText = (TextView) view.findViewById(R.id.alerttext);
+            }
+            public void UpdateUI(Context context,String data,int position){
+                buttonText.setText(data);
+                if (mDestructive!= null && mDestructive.contains(data)){
+                    buttonText.setTextColor(ContextCompat.getColor(context, R.color.negative));
+                }
+                else{
+                    buttonText.setTextColor(ContextCompat.getColor(context, R.color.positive));
+                }
+            }
+        }
     }
 }
