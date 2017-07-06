a6aee5d7ab44103c4ba904551d4860150b46a564
Ray <ray.li.verified@gmail.com>
2017-07-06 17:21:51 -0500

    Rename Sample App Package
    *Rename package of Sample App from customalertsample to customalertapp.

--- app/src/main/java/stream/customalertapp/MainActivity.java
+++ app/src/main/java/stream/customalertapp/MainActivity.java
@@ -1,13 +1,9 @@
-package stream.customalertsample;
+package stream.customalertapp;
 
 import android.app.Dialog;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
